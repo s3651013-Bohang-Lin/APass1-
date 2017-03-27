5d7dd2acf808ef7ce19ca43477a4813fa64a62e1
[rmit-s3651013-Bohang-Lin] <[s3651013@student.rmit.edu.au]>
2017-03-28 00:55:34 +1100

    Signed-off-by: [rmit-s3651013-Bohang-Lin] <[s3651013@student.rmit.edu.au]>

--- src/Driver.java
+++ src/Driver.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Driver {
+public class Driver {1
 	private Random read;
 	private int input;
 	private Scanner keyboard;
